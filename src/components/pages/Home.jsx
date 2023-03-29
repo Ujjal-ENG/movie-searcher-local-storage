63535de9b6b79ec9ac370728f61413b610f8adb3
user <ujjalroy7862@gmail.com>
2023-03-29 17:14:30 +0600

    done the card data showing

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SIngleCard from "./SIngleCard";
 
 const Home = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const fetchURL = await fetch("data.json");
+    const data = await fetchURL.json();
+    setData(data);
+  };
+
+  return (
+    <div className="">
+      {data && data.map((el, i) => <SIngleCard key={i} data={el} />)}
+    </div>
+  );
 };
 
 export default Home;
--- src/components/pages/SIngleCard.jsx
+++ src/components/pages/SIngleCard.jsx
@@ -1,26 +1,24 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-const SIngleCard = () => {
+const SIngleCard = (props) => {
+  const { description, imdbRating, movieName, poster, watchTime } = props.data;
   return (
-    <>
+    <div>
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        <Card.Img variant="top" src={poster} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Title>{movieName}</Card.Title>
+          <Card.Text>{description}</Card.Text>
         </Card.Body>
 
         <Card.Body className="d-flex gap-5">
-          <Card.Title>Watch Time</Card.Title>
-          <Card.Title>Rating</Card.Title>
+          <Card.Title>WatchTime: {watchTime}</Card.Title>
+          <Card.Title>Rating: {imdbRating}</Card.Title>
         </Card.Body>
       </Card>
       <Button variant="info my-2 w-25">Book Now</Button>
-    </>
+    </div>
   );
 };
 
