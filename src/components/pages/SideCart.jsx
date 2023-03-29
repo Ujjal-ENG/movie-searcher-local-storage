66d013928f562c26df02879dcbb6fe13eadf9516
user <ujjalroy7862@gmail.com>
2023-03-29 18:55:59 +0600

    set the button value into text field

--- src/components/pages/SideCart.jsx
+++ src/components/pages/SideCart.jsx
@@ -2,14 +2,45 @@ import React, { useEffect, useState } from "react";
 
 const SideCart = (props) => {
   const [time, setTime] = useState("");
+  const [breakTime, setBreakTime] = useState(0);
   useEffect(() => {
     const getWatchTime = localStorage.getItem("watchTime");
     setTime(getWatchTime);
   }, [props.data]);
 
+  const handleClick = (value) => {
+    setBreakTime(value);
+  };
+
   return (
     <div>
       <h1>Total WatchTime is: {time}</h1>
+      <h5 className="mt-5">Add Break time</h5>
+      <button
+        onClick={() => handleClick(15)}
+        className="w-25 btn-circle m-1 btn btn-info"
+      >
+        15
+      </button>
+      <button
+        className="w-25 btn-circle bg-warning btn btn-info"
+        onClick={() => handleClick(20)}
+      >
+        20
+      </button>
+      <button
+        className="w-25 btn-circle m-1 bg-danger btn btn-info"
+        onClick={() => handleClick(25)}
+      >
+        25
+      </button>
+      <input
+        type="text"
+        disabled={breakTime ? breakTime : true}
+        value={breakTime}
+        onChange={(e) => setBreakTime(e.target.value)}
+      />
+      <button className="mt-5 btn btn-info w-100">Complete</button>
     </div>
   );
 };
